1414433189a033f0cbf8ad10e2ce649569b60d0a
kentaro <taguchi-k@users.noreply.github.com>
2020-10-19 21:51:02 +0900

    Update Package.swift
    same version as Cartfile.resolved

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/drmohundro/SWXMLHash",
-            from: "5.0.1"
+            from: "4.7.6"
         )
     ],
     targets: [
